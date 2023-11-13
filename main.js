04ee3839f5bab9fcb6a714ab1490d0b4df7a4a13
amarmontrout <132516474+amarmontrout@users.noreply.github.com>
2023-11-12 22:39:38 -0600

    Button update/color change

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 document.getElementById('sub').addEventListener('click', subNumber);
 document.getElementById('add').addEventListener('click', addNumber);
 
-var number = document.getElementById('num');
+const number = document.getElementById('num');
 
 function subNumber() {
-    var num = parseInt(number.textContent);
-    var newNum = num - 1;
+    const num = parseInt(number.textContent);
+    const newNum = num - 1;
     number.textContent = newNum;
 }
 
 function addNumber() {
-    var num = parseInt(number.textContent);
-    var newNum = num + 1;
+    const num = parseInt(number.textContent);
+    const newNum = num + 1;
     number.textContent = newNum;
 }
